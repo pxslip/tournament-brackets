e6eadb8048a8b87e1ba395a6cb2f809341055b94
Will K (pxslip) <dev@pxslip.com>
2022-11-20 06:14:04 +0000

    Set up an actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+const base = process.env.VITE_BASE_PATH ?? '/';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base,
+  plugins: [vue()],
+});
